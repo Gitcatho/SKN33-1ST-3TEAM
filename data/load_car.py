9342fdb568b5e2cad3def5e08dee8d667965cd08
kimjeongjaeae <jj051232@gmail.com>
2026-06-17 09:54:21 +0900

    20260617_region, service center, manufacturer, car, recall

---
+++ data/load_car.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+df = pd.read_csv(r'C:\SKN_AI\SKN33-1ST-3TEAM\data\한국교통안전공단_차종별 리콜대수_20251231.csv', encoding='cp949')
+manufacturer_df = pd.read_csv(r'/data/manufacturer_df.csv', encoding='utf-8-sig')
+
+# 제작자 + 차명 중복 제거
+car_df = df[['제작자', '차명']].drop_duplicates().reset_index(drop=True)
+car_df['car_id'] = car_df.index + 1
+
+# manufacturer_id 매핑
+car_df = car_df.merge(manufacturer_df[['manufacturer_id', 'name']], left_on='제작자', right_on='name', how='left')
+car_df = car_df[['car_id', '차명', 'manufacturer_id']]
+car_df.columns = ['car_id', 'model_name', 'manufacturer_id']
+
+car_df.to_csv(r'C:\SKN_AI\SKN33-1ST-3TEAM\data\car_df.csv', index=False, encoding='utf-8-sig')
+print(f'총 {len(car_df)}개 차량 저장 완료')
+print(car_df.head(10))
